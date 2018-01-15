97adb01a58e27b76e10af87c43215efde76948ee
さとう <mokoroshi@gmail.com>
2018-01-15 11:02:38 +0900

    animation update

---
+++ Battle/Assets/Animation/motion/spine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spine : MonoBehaviour
+{
+
+    public Transform spineval;
+    public bool gun;
+    public Vector3 val;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        if (gun == true)
+        {
+            spineval.rotation = Quaternion.Euler(val);
+        }
+    }
+}
