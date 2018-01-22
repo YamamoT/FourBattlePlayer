efa27d216ec9e8d082803b0ba76616a5f440b2d2
yamamot <code_tatsuya@ezweb.ne.jp>
2018-01-22 11:34:17 +0900

    移動操作を変更

--- Battle/Assets/Scripts/Game/Player/PlayerStates.cs
+++ Battle/Assets/Scripts/Game/Player/PlayerStates.cs
@@ -7,9 +7,7 @@ public class PlayerStates : MonoBehaviour {
     [SerializeField][Range(0,100)]
     private int _hp; // 体力
     [SerializeField][Range(1f, 10f)]
-    private float _walkSpead; // 移動速度(歩き)
-    [SerializeField][Range(1f, 10f)]
-    private float _dushSpead; // 移動速度(走り)
+    private float _spead; // 移動速度
     [SerializeField][Range(1f, 10f)]
     private float _jumpPower; // ジャンプ力
     [SerializeField]
@@ -124,13 +122,9 @@ public class PlayerStates : MonoBehaviour {
         set { _hp = value; }
     }
     
-    public float WalkSpd
+    public float Spead
     {
-        get { return _walkSpead; }
-    }
-    public float DushSpd
-    {
-        get { return _dushSpead; }
+        get { return _spead; }
     }
     public float JumpPow
     {
