f292c75a6ecfb1928a4f1fc6da85e2910e5eb377
S.T <raincat8618@gmail.com>
2018-01-29 12:01:17 +0900

    シーン遷移のマネージャー(※gManager.cs)とデバッグ用のシーン4種を追加

---
+++ Battle/Assets/Scripts/Manager/gManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SceneState
+{
+    Title,
+    Play,
+    Result,
+    Config
+}
+
+public class gManager : MonoBehaviour
+{
+    // 
+    public static gManager instance;
+    // 現在のシーン
+    private SceneState currentState;
+
+    // Use this for initialization
+    void Start()
+    {
+        instance = this;
+        // シーンを跨いでもこのスクリプトがアタッチされているオブジェクトを破棄しない
+        DontDestroyOnLoad(this.gameObject);
+        // 最初のシーンをタイトルに設定
+        currentState = SceneState.Title;
+    }
+
+    public void SetCurrentState(SceneState state)
+    {
+        currentState = state;
+        OnSceneChanged(currentState);
+    }
+
+    void OnSceneChanged(SceneState state)
+    {
+        switch (state)
+        {
+            case SceneState.Title:
+                TitleAction();
+                break;
+            case SceneState.Play:
+                PlayAction();
+                break;
+            case SceneState.Result:
+                ResultAction();
+                break;
+            case SceneState.Config:
+                ConfigAction();
+                break;
+        }
+    }
+
+    void TitleAction()
+    {
+        SceneManager.LoadScene("TestTitleScene");
+    }
+
+    void PlayAction()
+    {
+        SceneManager.LoadScene("TestPlayScene");
+    }
+
+    void ResultAction()
+    {
+        SceneManager.LoadScene("TestResultScene");
+    }
+
+    void ConfigAction()
+    {
+        SceneManager.LoadScene("TestConfigScene");
+    }
+}
---
+++ Battle/Assets/Scripts/TestScenes/TestTitleScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTitleScene : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            gManager.instance.SetCurrentState(SceneState.Play);
+        }
+        else if(Input.GetKeyDown(KeyCode.R))
+        {
+            gManager.instance.SetCurrentState(SceneState.Result);
+        }
+        else if(Input.GetKeyDown(KeyCode.T))
+        {
+            gManager.instance.SetCurrentState(SceneState.Title);
+        }
+        else if(Input.GetKeyDown(KeyCode.C))
+        {
+            gManager.instance.SetCurrentState(SceneState.Config);
+        }
+            
+	}
+}
