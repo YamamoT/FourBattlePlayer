937db43da2479e626717ffc3ebcaaf7929910f22
さとう <mokoroshi@gmail.com>
2018-01-29 15:06:55 +0900

    bug fix

--- Battle/Assets/Scripts/Game/Weapon/bulletscale.cs
+++ Battle/Assets/Scripts/Game/Weapon/bulletscale.cs
@@ -10,7 +10,6 @@ public class bulletscale : MonoBehaviour {
         this.transform.localScale = new Vector3(this.transform.parent.localScale.x * this.transform.localScale.x,
                                         this.transform.parent.localScale.y * this.transform.localScale.y,
                                         this.transform.parent.localScale.z * this.transform.localScale.z);
-
     }
 
     // Update is called once per frame
