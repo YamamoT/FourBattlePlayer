9f543d7c0bb891d192845e9928566d90e7b4ae1d
Syunpei <grean1325@icloud.com>
2018-02-02 14:33:55 +0900

    パンチした時の吹っ飛ばす処理

---
+++ Battle/Assets/Oishi/Scripts/Punch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punch : MonoBehaviour {
+    float speed = 30.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            Vector3 velocity = (other.transform.position - transform.position).normalized * speed;
+            rb.AddForce(velocity * speed);
+        }
+    }
+}
