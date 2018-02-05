493200e177610718dbc058bdee4eaffe3839a886
Syunpei <grean1325@icloud.com>
2018-02-05 13:30:06 +0900

    落下時のダメージの追加

--- Battle/Assets/Oishi/Scripts/Fall.cs
+++ Battle/Assets/Oishi/Scripts/Fall.cs
@@ -18,7 +18,9 @@ public class Fall : MonoBehaviour {
     {
         if(other.tag == "Player")
         {
+            PlayerStates pStates = other.GetComponent<PlayerStates>();
             other.transform.position = new Vector3(0, 15, 0);
+            pStates.Hp -= 20;
         }
     }
 }
