14ce8423948f6a5ef7a14a138efce717d2a943ba
さとう <mokoroshi@gmail.com>
2018-02-05 14:36:41 +0900

    ステージ関連

---
+++ Battle/Assets/Scripts/Manager/StageManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour {
+
+    public GameObject[] _stage;
+
+    private int _stageNum;
+
+	// Use this for initialization
+	void Awake () {
+
+        _stageNum = Random.Range(0, _stage.Length);
+
+        Instantiate(_stage[_stageNum]);
+        		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
