58db6ab09b691c52c32c93938f8349f88cd5e104
さとう <mokoroshi@gmail.com>
2018-02-08 14:38:52 +0900

    BGM追加&キャラセレ画像追加

---
+++ Battle/Assets/Scripts/Manager/BGMPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMPlayer : MonoBehaviour {
+
+    //サウンド
+    public AudioSource Sound;
+    public AudioClip BGM;
+
+    // Use this for initialization
+    void Start () {
+        //サウンド再生
+        Sound.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
